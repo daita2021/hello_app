90a932df5fe42e8924fcb36a9659b0641b1c6230
Daita <your.email@example.com>
2021-07-14 01:32:05 +0000

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within thisfile, see https://guides.rubyonrng.html
-  root 'application#goodbye'
+  root 'application#hello'
 end
